d9eff1c15f9378deb1b1a9d2355aa7624a2cf040
DipuKP2001 <dipukp602@gmail.com>
2021-03-25 20:30:18 +0530

    alanBtn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-const App = () => {}
+import React,{useEffect} from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: process.env.REACT_APP_ALAN_API_KEY,
+            onCommand: ({ command }) => {
+                if( command === 'test' ) {
+                    alert('Test was executed')
+                }
+            }
+        })
+    },[])
+
+    return(
+        <div>
+            <h1>This is Alan AI</h1>
+        </div>
+    )
+}
 
 export default App
